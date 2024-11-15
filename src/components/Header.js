04b41a20c1e78735d838f0bb31c266ef834d2ba3
parkseyoung1209 <rodlsdyd4@naver.com>
2024-11-15 13:01:40 +0900

    1115
    개인 공부 홈페이지 첫 시작

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    console.log("Header 렌더링 중");
+    return (
+        <>
+            <div>
+                헤더 테스트 1
+            </div>
+        </>
+    );
+};
+
+export default Header;
---
+++ src/pages/Main.js
@@ -0,0 +1,10 @@
+const Main = () => {
+
+    return (
+        <div>
+            메인 테스트 1
+        </div>
+    );
+};
+
+export default Main;
